8b754332e7d33be2c7e88ad23918ca7367e3d780
choco-momo <chocomo-am001@nigrescens.sakura.ne.jp>
2026-05-11 05:35:13 +0900

    Update character details in BloodlineGenerator
    レフィル固有ID変更R1→O3

--- src/BloodlineGenerator.jsx
+++ src/BloodlineGenerator.jsx
@@ -18,9 +18,9 @@ const initialCharacters = [
   { id: "15", name: "ルーグ",          code: "J1", gender: "M",    father: "K2", mother: "O1", outsider: false, pairId: "",   pairRole: "",  deceased: false },
   { id: "16", name: "シンシア",        code: "J2", gender: "F",    father: "K2", mother: "O1", outsider: false, pairId: "08", pairRole: "F", deceased: false },
   { id: "17", name: "ナール",          code: "V1", gender: "M",    father: "",   mother: "",   outsider: true,  pairId: "09", pairRole: "M", deceased: false },
-  { id: "18", name: "レフィル",        code: "R1", gender: "F",    father: "A1", mother: "D1", outsider: false, pairId: "09", pairRole: "F", deceased: false },
-  { id: "19", name: "ナハル",          code: "H1", gender: "M",    father: "V1", mother: "R1", outsider: false, pairId: "",   pairRole: "",  deceased: false },
-  { id: "20", name: "アシェラ",        code: "H2", gender: "F",    father: "V1", mother: "R1", outsider: false, pairId: "10", pairRole: "F", deceased: false },
+  { id: "18", name: "レフィル",        code: "O3", gender: "F",    father: "A1", mother: "D1", outsider: false, pairId: "09", pairRole: "F", deceased: false },
+  { id: "19", name: "ナハル",          code: "H1", gender: "M",    father: "V1", mother: "O3", outsider: false, pairId: "",   pairRole: "",  deceased: false },
+  { id: "20", name: "アシェラ",        code: "H2", gender: "F",    father: "V1", mother: "O3", outsider: false, pairId: "10", pairRole: "F", deceased: false },
   { id: "21", name: "リリー",          code: "X1", gender: "F",    father: "",   mother: "",   outsider: true,  pairId: "98", pairRole: "F", deceased: false },
   { id: "22", name: "ライラ",          code: "Z1", gender: "F",    father: "",   mother: "",   outsider: true,  pairId: "99", pairRole: "F", deceased: true },
 ];
